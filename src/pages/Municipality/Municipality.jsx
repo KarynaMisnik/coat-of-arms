8dbd6b7b517794d3292cf1e35575d4a8fde42fd7
KarynaMisnik <karinamisnik94@gmail.com>
2023-12-18 19:33:13 +0200

    About, Quiz pages created, Navbar navigation added(Routes)
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

---
+++ src/pages/Municipality/Municipality.jsx
@@ -0,0 +1,9 @@
+const Municipality = () => {
+  return (
+    <>
+      <h1>This is Municipality page</h1>
+    </>
+  );
+};
+
+export default Municipality;
---
+++ src/pages/Quiz/Quiz.jsx
@@ -0,0 +1,9 @@
+const Quiz = () => {
+  return (
+    <>
+      <h1>This is Quiz page</h1>
+    </>
+  );
+};
+
+export default Quiz;
