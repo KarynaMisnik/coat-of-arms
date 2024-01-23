81d397ddf8c170ef61a12eacc8b7161a9f0becec
KarynaMisnik <karinamisnik94@gmail.com>
2024-01-23 11:03:03 +0200

    base added in main.js hashrouter and html
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
+  <Router basename="/coat-of-arms/">
     <App />
   </Router>
 );
