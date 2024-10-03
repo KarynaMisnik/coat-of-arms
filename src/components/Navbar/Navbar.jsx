9e3b99ff9d97210eca701b1782338ba3d29d13d7
KarynaMisnik <karinamisnik94@gmail.com>
2024-10-03 21:46:04 +0300

    working on search bar, look comments
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,11 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { NavLink } from "react-router-dom";
-
+/* Search bar 
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+*/
 const navText = {
   color: "var(--white)",
   fontSize: "1.2rem",
@@ -32,6 +36,49 @@ export default function Navbar(props) {
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
+  /* for search bar
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: "8px",
+    backgroundColor: "blue",
+    "&:hover": {
+      backgroundColor: "yellow",
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    width: "100%",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      [theme.breakpoints.up("sm")]: {
+        width: "12ch",
+        "&:focus": {
+          width: "20ch",
+        },
+      },
+    },
+  }));
+  */
 
   const drawer = (
     <Box
@@ -76,6 +123,18 @@ export default function Navbar(props) {
           borderBottom: "3px solid var( --white)",
         }}
       >
+        {/*
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        */}
+
         <Toolbar>
           <IconButton
             color="inherit"
