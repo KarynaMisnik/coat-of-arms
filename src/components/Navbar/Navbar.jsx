4d2444daf669f12024b0658d1b9e8bb7c0112586
KarynaMisnik <karinamisnik94@gmail.com>
2024-10-10 16:06:20 +0300

    Search bar, needs fixes
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,11 +14,10 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { NavLink } from "react-router-dom";
-/* Search bar 
-import SearchIcon from "@mui/icons-material/Search";
+
 import { styled, alpha } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
-*/
+
 const navText = {
   color: "var(--white)",
   fontSize: "1.2rem",
@@ -36,39 +35,35 @@ export default function Navbar(props) {
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
-  /* for search bar
+
   const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: "8px",
+    borderRadius: "50px",
+    padding: 0,
     backgroundColor: "blue",
     "&:hover": {
       backgroundColor: "yellow",
     },
-    marginLeft: 0,
+    margin: 0,
     width: "100%",
     [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
       width: "auto",
     },
   }));
 
   const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
     height: "100%",
     position: "absolute",
     pointerEvents: "none",
     display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
   }));
 
   const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: "inherit",
     width: "100%",
     "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
+      margin: "0 10px",
       // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+
       transition: theme.transitions.create("width"),
       [theme.breakpoints.up("sm")]: {
         width: "12ch",
@@ -78,7 +73,6 @@ export default function Navbar(props) {
       },
     },
   }));
-  */
 
   const drawer = (
     <Box
@@ -123,20 +117,6 @@ export default function Navbar(props) {
           borderBottom: "3px solid var( --white)",
         }}
       >
-        {/* Add filter method or smth: go through data to show result munisipalities and regions 
-
-
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
-        */}
-
         <Toolbar>
           <IconButton
             color="inherit"
@@ -147,6 +127,7 @@ export default function Navbar(props) {
           >
             <MenuIcon />
           </IconButton>
+
           <Typography
             sx={{ display: { xs: "flex", sm: "none", md: "none", xl: "none" } }}
             {...navText}
@@ -162,6 +143,15 @@ export default function Navbar(props) {
           >
             Finnish Emblem
           </Typography>
+          {/* SEARCH BAR HERE */}
+          <Search>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          {/* SEARCH BAR ENDS HERE */}
+
           <Box sx={{ display: { xs: "none", sm: "block" } }}>
             {navItems.map((item) => (
               <NavLink className="nav-link" to={`/${item.toLowerCase()}`}>
