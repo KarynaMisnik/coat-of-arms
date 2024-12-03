1f60f3f865100e8d89ed155ee274d900833be90c
KarynaMisnik <karinamisnik94@gmail.com>
2024-12-03 22:22:42 +0200

    About page: sall updates
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,20 +1,24 @@
+import Paper from "@mui/material/Paper";
+
 const About = () => {
   return (
     <div className="about-page-wrapper">
-      <h1 className="about-page-heading">Finnish Emblem</h1>
-      <p className="about-page-text">
-        This project is dedicated to exploring the rich regional tapestry of
-        Finland! Immerse yourself in the fascinating details of Finland's 19
-        regions, each adorned with its unique coat of arms. The country has
-        undergone various historical events that have shaped it, and these
-        events have given rise to specific cultural and traditional traits,
-        reflected in the coat of arms. The project provides a brief description
-        of each emblem, including when it was formed and by whom. You can find
-        current information on the population, weather, and location of each
-        region and municipality. For a fun twist, test your knowledge with the
-        interactive quiz, challenging you to match regions and municipalities
-        with their distinctive coat of arms.
-      </p>
+      <Paper className="about-page-content-container">
+        <h1 className="about-page-heading">Finnish Emblem</h1>
+        <p className="about-page-text">
+          This project is dedicated to exploring the rich regional tapestry of
+          Finland! Immerse yourself in the fascinating details of Finland's 19
+          regions, each adorned with its unique coat of arms. The country has
+          undergone various historical events that have shaped it, and these
+          events have given rise to specific cultural and traditional traits,
+          reflected in the coat of arms. The project provides a brief
+          description of each emblem, including when it was formed and by whom.
+          You can find current information on the population, weather, and
+          location of each region and municipality. For a fun twist, test your
+          knowledge with the interactive quiz, challenging you to match regions
+          and municipalities with their distinctive coat of arms.
+        </p>
+      </Paper>
     </div>
   );
 };
